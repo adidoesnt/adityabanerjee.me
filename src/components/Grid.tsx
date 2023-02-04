c4454d07bad81ee0b0dbf36b71953c4d4087b79a
Aditya Banerjee <aditya_banerjee@icloud.com>
2023-02-04 23:56:47 +0800

    add portrait

--- src/components/Grid.tsx
+++
@@ -1,9 +0,0 @@
-export type GridProps = {
-    style: object;
-}
-
-export default function Grid({ style }: GridProps) {
-    return <div style={style} >
-
-    </div>
-}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,39 @@
-import Head from 'next/head';
+import Head from "next/head";
+import { Grid, GridItem } from "@chakra-ui/react";
+import { Portrait } from "@/assets/portrait";
+
+function Bio() {
+  return <Grid gridTemplateColumns={'1fr 2fr'}>
+    <GridItem>
+      <Portrait />
+    </GridItem>
+    <GridItem>
+      Hello
+    </GridItem>
+  </Grid>
+}
+
+function Body() {
+  return (
+    <Grid
+      gridTemplateRows={"repeat(3, 1fr)"}
+      w={"100vw"}
+      h={"100vh"}
+      justifyContent={"center"}
+      alignItems={"center"}
+    >
+      <GridItem w={"100%"} h={"auto"}>
+        Hello
+      </GridItem>
+      <GridItem w={"100%"} h={"auto"}>
+        <Bio />
+      </GridItem>
+      <GridItem w={"100%"} h={"auto"}>
+        Hello
+      </GridItem>
+    </Grid>
+  );
+}
 
 export default function Home() {
   return (
@@ -7,8 +42,8 @@ export default function Home() {
         <title>Aditya Banerjee</title>
       </Head>
       <main>
-        <div>This is my website.</div>
+        <Body />
       </main>
     </>
-  )
+  );
 }
