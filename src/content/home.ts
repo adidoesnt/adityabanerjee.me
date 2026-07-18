ab23b3f52ceb435f79b7c9bc106f642693bf728c
Aditya Banerjee <aditya_banerjee@icloud.com>
2026-07-19 00:37:32 +0100

    add homepage

---
+++ src/content/home.ts
@@ -0,0 +1,9 @@
+export interface HomeContent {
+	displayImage: string;
+	tagline: string[];
+}
+
+export const home: HomeContent = {
+	displayImage: '/adi.jpg',
+	tagline: ['Full-time developer, part-time musician.', 'JavaScript is my poison of choice.'],
+};
