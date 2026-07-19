86d5c4ecb18fd3bafbb4169eafb5b4fcd0fc6c57
Aditya Banerjee <aditya_banerjee@icloud.com>
2026-07-19 17:02:03 +0100

    add widgets

---
+++ src/content/spotify.ts
@@ -0,0 +1,9 @@
+export interface SpotifyContent {
+	artistId: string;
+	playlistId: string;
+}
+
+export const spotify: SpotifyContent = {
+	artistId: '652hYXo0CZjc2zHEzUsWsb',
+	playlistId: '14KWqSO6j7clrBhPPvkbZQ',
+};
