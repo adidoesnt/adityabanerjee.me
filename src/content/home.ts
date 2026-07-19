7bdeabb1240b94c44fbba0219deea0d0f37a38c5
Aditya Banerjee <aditya_banerjee@icloud.com>
2026-07-19 17:25:01 +0100

    add date to header and remove auto-open of whomai

--- src/content/home.ts
+++ src/content/home.ts
@@ -1,9 +1,13 @@
 export interface HomeContent {
 	displayImage: string;
 	tagline: string[];
+	role: string;
+	education: string;
 }
 
 export const home: HomeContent = {
 	displayImage: '/adi.jpg',
 	tagline: ['Full-time developer, part-time musician.', 'JavaScript is my poison of choice.'],
+	role: 'Fullstack AI Engineer @ Spotify',
+	education: "NUS Computer Science '23",
 };
